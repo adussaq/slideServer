fac321c11934f12f39653ebf328a1008764bc2dd
Alex Dussaq <adussaq@Alexs-MacBook-Pro.local>
2021-04-09 10:57:53 -0700

    change port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 	server.get('/hello/:name', respond);
 	server.head('/hello/:name', respond);
 
-	server.listen(8080, function() {
+	server.listen(80, function() {
 	  console.log('%s listening at %s', server.name, server.url);
 	});
 }())
